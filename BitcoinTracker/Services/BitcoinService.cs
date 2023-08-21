571011c2f4bc8fcfb666a6f778b993647fce49d8
cwhitelam <cwhitelam70@gmail.com>
2023-08-21 17:56:19 -0400

    Add project files.

---
+++ BitcoinTracker/Services/BitcoinService.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace BitcoinTracker.Services;
+
+public class BitcoinService
+{
+    private readonly HttpClient httpClient;
+
+    public BitcoinService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<decimal?> GetCurrentPriceAsync()
+    {
+        try
+        {
+            var response = await httpClient.GetStringAsync("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+            var priceData = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, decimal>>>(response);
+            return priceData!["bitcoin"]["usd"];
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while fetching the Bitcoin price: {ex.Message}");
+            return null;
+        }
+    }
+}
